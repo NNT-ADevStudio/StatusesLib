eae24cd9415c7d2d108af8c2db6e702a4f8cc84e
lAIRlRI <renal_adamenko@mail.ru>
2024-02-20 22:01:26 +0400

    Добавьте файлы проекта.

---
+++ StatusesLib/Models/Status.cs
@@ -0,0 +1,21 @@
+﻿namespace StatusesLib.Models
+{
+    public class Status<T>
+    {
+        public virtual int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public T Value { get; set; }
+
+        public Status() { }
+
+        public Status(int id, string name) => (Id, Name) = (id, name);
+
+        public Status(int id, string name, T value) : this(id, name) => Value = value;
+
+        public Status(int id, string name, T value, string description) : this(id, name, value) => Description = description;
+    }
+}
---
+++ StatusesLib/Models/StatusError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StatusesLib.Models
+{
+    internal class StatusError<T> : Status<T>
+    {
+        public Exception Exception { get; set; }
+
+        public StatusError(int id, string name, Exception ex) : base(id, name) => Exception = ex;
+
+        public StatusError(int id, string name, T value, Exception ex) : base(id, name, value) => Exception = ex;
+
+        public StatusError(int id, string name, T value, string message, Exception ex) : base(id, name, value, message) => Exception = ex;
+    }
+}
