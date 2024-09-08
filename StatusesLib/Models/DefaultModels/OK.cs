73b289140945f87704778a130f4655f8dfffed8f
lAIRlRI <renal_adamenko@mail.ru>
2024-09-08 16:36:26 +0400

    add constructor OK

--- StatusesLib/Models/DefaultModels/OK.cs
+++ StatusesLib/Models/DefaultModels/OK.cs
@@ -9,5 +9,11 @@
         public override string Name => "OK";
 
         public override TypeStatus Type => TypeStatus.OK;
+
+        public OK(string name) : base(name) { }
+
+        public OK(string name, T value) : base(name, value) { }
+
+        public OK(string name, T value, string message) : base(name, value, message) { }
     }
 }
